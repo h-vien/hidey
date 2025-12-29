0cd7cb840be3bf6ae1434221cbc090811110b1d2
h-vien <vien200203@gmail.com>
2025-12-29 14:47:32 +0700

    style: rebranding

--- src/content/floating-button.ts
+++ src/content/floating-button.ts
@@ -81,7 +81,7 @@ class FloatingButton {
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10"/></svg>
       </div>
       <span class="hidey-btn-close" aria-label="Close and disable blur" title="Close and disable blur">
-      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg></span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="#1976d2" stroke="#1976d2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg></span>
     `;
 
     // Close button handler
